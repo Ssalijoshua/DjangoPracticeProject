af23fcc54db0231cf0cc42888455ce89a18b71a3
Ssalijoshua <ssalijoshua2002@gmail.com>
2025-08-19 20:32:31 +0300

    Rewriting the project

--- mysite/account/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import UserProfile
-
-class UserProfileAdmin(admin.ModelAdmin):
-    list_display = ('user', 'birth', 'phone')
-    list_filter = ("phone",)
-
-admin.site.register(UserProfile, UserProfileAdmin)
-
